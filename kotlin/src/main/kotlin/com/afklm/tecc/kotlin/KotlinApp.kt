d67588950aa35fd56186e7df7e5b608ff36c3abc
Marc Guerrini <maguerrini@airfrance.fr>
2019-10-24 16:28:01 +0200

    event listener annotation

--- kotlin/src/main/kotlin/com/afklm/tecc/kotlin/KotlinApp.kt
+++ kotlin/src/main/kotlin/com/afklm/tecc/kotlin/KotlinApp.kt
@@ -1,11 +1,42 @@
 package com.afklm.tecc.kotlin
 
-import org.springframework.boot.runApplication
+import com.afklm.tecc.kotlin.data.Article
+import com.afklm.tecc.kotlin.data.ArticleRepository
+import com.afklm.tecc.kotlin.data.User
+import com.afklm.tecc.kotlin.data.UserRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.boot.runApplication
+import org.springframework.context.event.EventListener
 
 @SpringBootApplication
-open class KotlinApp
+open class KotlinApp(
+    private val articleRepository: ArticleRepository,
+    private val userRepository: UserRepository
+) {
 
-fun main(args: Array<String>) {
-    runApplication<KotlinApp>(*args)
+    fun main(args: Array<String>) {
+        runApplication<KotlinApp>(*args)
+    }
+
+    @EventListener
+    fun initDb(e: ApplicationReadyEvent) {
+        var me = userRepository.save(User("maguerrini", "Marc", "Guerrini"))
+        articleRepository.save(
+            Article(
+                title = "Reactor Bismuth is out",
+                headline = "Lorem ipsum",
+                content = "dolor sit amet",
+                author = me
+            )
+        )
+        articleRepository.save(
+            Article(
+                title = "Reactor Aluminium has landed",
+                headline = "Lorem ipsum",
+                content = "dolor sit amet",
+                author = me
+            )
+        )
+    }
 }
